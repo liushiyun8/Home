4985b90f7e787a80153c65c1e5391909b25952a6
liuyun <764512727@qq.com>
2017-11-13 17:40:31 +0800

    新的

--- app/src/main/java/com/liuyun/day30lianxi/MainActivity.java
+++ app/src/main/java/com/liuyun/day30lianxi/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
         //这个是服务器
         //呵呵呵。
-        //本地的服务区
+        //本地
     }
     
 
