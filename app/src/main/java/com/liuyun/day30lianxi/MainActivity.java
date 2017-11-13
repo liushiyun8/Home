6edfaf71701c24f662ff5ea9ddad3eb4889dc6f8
liuyun <764512727@qq.com>
2017-11-13 15:37:05 +0800

    合并测试

--- app/src/main/java/com/liuyun/day30lianxi/MainActivity.java
+++ app/src/main/java/com/liuyun/day30lianxi/MainActivity.java
@@ -3,6 +3,7 @@ package com.liuyun.day30lianxi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -11,12 +12,13 @@ import com.bumptech.glide.Glide;
 public class MainActivity extends AppCompatActivity {
 
     private EditText image;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         image = (EditText) findViewById(R.id.image);
-
+        button = (Button) findViewById(R.id.button);
     }
 }
